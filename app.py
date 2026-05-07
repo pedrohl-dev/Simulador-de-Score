e1d540d6299953dda92e6a0f80634065c248b53a
Pedro Henrique <pedrolimapedrolima100@gmail.com>
2026-05-07 20:25:15 +0000

    corrigindo metodo POST

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import random
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     score = None
     limite = None
